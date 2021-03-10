7a525e7e7afa65365e0771bc1fab282af4625f0c
Kaushik Madapati <ibmwatsonchampion777@gmail.com>
2021-03-10 15:50:23 -0800

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,15 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c2da10aeabd854e45b53b7b4d48c0f53&query=' + longitude + ',' + latitude + '&units=f'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location!', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+}
+module.exports = forecast
